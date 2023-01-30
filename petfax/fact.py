2fa084d505d3a2fed27d1df82e82132156a7ab3e
jeffdonkey <8jefa8@gmail.com>
2023-01-30 09:27:43 -0500

    day6 act1 done and working

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,7 +1,19 @@
-from flask import ( Blueprint, render_template ) 
+from flask import ( Blueprint, render_template, request, redirect ) 
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
+
+# BELOW IS A ROUTE TO CONNCECTS TO 'templates/facts/new.html'
+# THE ROUTE ACCEPTS METHODS 'GET' AND 'POST'
+
+@bp.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        print(request.form)
+        return redirect('/facts')    
+    
+    return render_template('/facts/index.html')
 
 @bp.route('/new')
 def new(): 
     return render_template('facts/new.html')
+
